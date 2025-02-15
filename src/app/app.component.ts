31ce79b6ef87b3ef2a9d0acd3ab624fa38b7bf9f
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-02-15 18:17:31 +0100

    feat: add a basic table to test Spring API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from "./components/products-list/products-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
