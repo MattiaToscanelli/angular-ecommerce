0006615a130f37e5b6b24f561c6bd5eb3b1cd419
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-03-13 19:51:31 +0100

    feat: added dynamic category menu by API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 //import { ProductsListComponent } from "./components/products-list/products-list.component";
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, ProductCategoryMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { map } from 'rxjs/operators';
 export class ProductService {
 
   private baseURL = 'http://localhost:8080/api/products';
+  private categoryURL = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) {  }
 
@@ -17,14 +19,28 @@ export class ProductService {
 
     const searchURL = `${this.baseURL}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchURL).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchURL).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories() : Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductsCategory>(this.categoryURL).pipe(
+      map(response => response._embedded.productCategory)
+    );
+
+  }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductsCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
