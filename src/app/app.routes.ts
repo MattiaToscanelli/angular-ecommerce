20044ed793917a4c9f5560802920aadaeaa4c7ff
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-03-22 14:38:10 +0100

    feat: added shopping cart details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 export const routes: Routes = [    
     { path: 'products/:id', component: ProductDetailsComponent },
@@ -8,6 +9,7 @@ export const routes: Routes = [
     { path: 'category/:id', component: ProductsListComponent },
     { path: 'category', component: ProductsListComponent },
     { path: 'products', component: ProductsListComponent },
+    { path: 'cart-details', component: CartDetailsComponent},
     { path: '', redirectTo: '/products', pathMatch: 'full' },
     { path: '**', redirectTo: '/products', pathMatch: 'full' },
 ];
