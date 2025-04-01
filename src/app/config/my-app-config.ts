d26dc96fa6e94f6707d421f18d2fce5c063e6292
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-04-01 18:39:34 +0200

    test: .gitignore

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc : {
-        clientId: '<<ADD-YOUR-CLIENT-ID>>>',
+        clientId: 'test',
         issuer: 'https://<<ADD-YOUR-DEV-DOMAIN>>/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
