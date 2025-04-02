08c8dd0e6da08de77a51311b99589d6417448874
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-04-02 18:06:23 +0200

    fix: double login with incognito

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,20 +2,20 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import OktaSignIn from '@okta/okta-signin-widget';
-
+import { Router } from '@angular/router';
 import myAppConfig from '../../config/my-app-config';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
   oktaSignIn: any;
 
-  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth,
+              private router: Router) {
     this.oktaSignIn = new OktaSignIn({
       logo: 'assets/images/logo.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
@@ -31,19 +31,42 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.oktaSignIn.remove();
+    // Gestisci il ritorno da Okta se siamo nel callback
+    if (window.location.pathname === '/login/callback') {
+      this.oktaAuth.handleLoginRedirect().then(() => {
+        // Dopo aver gestito il login redirect, naviga alla home
+        this.router.navigate(['/']);
+      }).catch(err => {
+        console.error('Errore nel gestire il login redirect:', err);
+      });
+      return;
+    }
 
-    this.oktaSignIn.renderEl(
-      { el: '#okta-sign-in-widget' },
-      (response: any) => {
-        if (response.status === 'SUCCESS') {
-          this.oktaAuth.signInWithRedirect();
-        }
-      },
-      (err: any) => {
-        throw err;
+    // Se non siamo nel callback, controlliamo se l'utente è già autenticato
+    this.oktaAuth.isAuthenticated().then(authenticated => {
+      if (authenticated) {
+        // Se l'utente è già autenticato, naviga alla home
+        this.router.navigate(['/']);
+      } else {
+        // Se l'utente non è autenticato, mostra il widget di login Okta
+        this.oktaSignIn.renderEl(
+          { el: '#okta-sign-in-widget' },
+          (response: any) => {
+            if (response.status === 'SUCCESS') {
+              // Gestisci il login direttamente senza signInWithRedirect
+              this.oktaAuth.tokenManager.setTokens(response.tokens);
+              this.oktaSignIn.remove();
+              this.router.navigate(['/']);
+
+              // TODO: TEST with Https
+              // this.oktaAuth.signInWithRedirect();
+            }
+          },
+          (err: any) => {
+            console.error('Errore nel widget Okta:', err);
+          }
+        );
       }
-    );
+    });
   }
-
 }
